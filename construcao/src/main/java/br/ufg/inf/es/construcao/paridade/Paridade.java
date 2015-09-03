1a6025640e7c7d8b1c53c881c93a1fd63cbfdf2e
Jhordan <jhordangabriel@hotmail.com>
2015-09-03 19:53:55 -0300

    adicionando classe Paridade e testes

--- construcao/src/main/java/br/ufg/inf/es/construcao/paridade/Paridade.java
+++ construcao/src/main/java/br/ufg/inf/es/construcao/paridade/Paridade.java
@@ -1,16 +1,20 @@
+package br.ufg.inf.es.construcao.paridade;
+
 /*
  * Implementação para verificação se inteiro é par ou não.
  */
-package br.ufg.inf.es.construcao.paridade;
-
-/**
- *
- * @author Jhordan
- */
 public class Paridade {
 
+    /**
+     *
+     * Verifica paridade de um número
+     *
+     * @param numero Número cuja paridade é verificada
+     * @return {@code true} se o número fornecio é par e {@code false},caso contrário.
+     *
+     */
     public static boolean verifica(int numero) {
-        return (numero)%2 == 0;
+        return (numero) % 2 == 0;
     }
 
 }
