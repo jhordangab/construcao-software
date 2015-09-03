7846e9202dd490d5da17cc5142e7392c20190fb0
Jhordan <jhordangabriel@hotmail.com>
2015-09-03 19:35:12 -0300

    adicionando pacote paridade

---
+++ construcao/src/main/java/br/ufg/inf/es/construcao/paridade/Paridade.java
@@ -0,0 +1,16 @@
+/*
+ * Implementação para verificação se inteiro é par ou não.
+ */
+package br.ufg.inf.es.construcao.paridade;
+
+/**
+ *
+ * @author Jhordan
+ */
+public class Paridade {
+
+    public static boolean verifica(int numero) {
+        return (numero)%2 == 0;
+    }
+
+}
