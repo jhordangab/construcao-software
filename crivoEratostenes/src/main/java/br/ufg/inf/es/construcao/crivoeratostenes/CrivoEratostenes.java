8bb816fd302e1f7123147139141b3bfadcf0b292
jhordan <jhordangabriel@hotmail.com>
2015-09-17 21:08:59 -0300

    adicionando testes ao Crivo

--- crivoEratostenes/src/main/java/br/ufg/inf/es/construcao/crivoeratostenes/CrivoEratostenes.java
+++ crivoEratostenes/src/main/java/br/ufg/inf/es/construcao/crivoeratostenes/CrivoEratostenes.java
@@ -1,19 +1,31 @@
 package br.ufg.inf.es.construcao.crivoeratostenes;
 
-/**
- *
- * @author Jhordan
- */
 public class CrivoEratostenes {
 
     /**
-     * Retorna números primos
+     * Retorna números primos de determinado array
      *
+     * @param array
+     * @param quantidade
+     * @return array com os números primos
      */
-    public static int[] crivoEratostenes(int array[], int quantidade) {
+    public static int[] crivoEratostenes(int[] array, int quantidade) {
 
-        if (quantidade < 1) {
-            throw new IllegalArgumentException("'quantidade' não pode ser menor que 1");
+        if (quantidade < 2) {
+            throw new IllegalArgumentException("'quantidade' não pode ser menor"
+                    + " que 2");
+        }
+
+        if (array.length != quantidade) {
+            throw new IllegalArgumentException("O tamanho do array é diferente "
+                    + "da quantidade informada");
+        }
+
+        for (int x = 2; x < quantidade; x++) {
+            if (array[x] != 0) {
+                throw new IllegalArgumentException("Valores a partir da segunda"
+                        + " posição do array não podem ser diferentes de zero");
+            }
         }
 
         int i = 2;
@@ -22,10 +34,9 @@ public class CrivoEratostenes {
                 int c = i + i;
                 while (c <= quantidade) {
                     array[c] = 1;
-                    c++;
+                    c = c + i;
                 }
             }
-            System.out.println(array[i]);
             i++;
         }
         return array;
--- crivoEratostenes/src/test/java/br/ufg/inf/es/construcao/crivoeratostenes/CrivoEratostenesTest.java
+++ crivoEratostenes/src/test/java/br/ufg/inf/es/construcao/crivoeratostenes/CrivoEratostenesTest.java
@@ -12,14 +12,38 @@ import static org.junit.Assert.*;
  * @author Jhordan
  */
 public class CrivoEratostenesTest {
-    
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void testArrayDiferenteQuantidade() {
+        int quantidade = 10;
+        int[] array = new int[8];
+        CrivoEratostenes.crivoEratostenes(array, quantidade);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void testQuantidadeMenor2() {
+        int quantidade = 1;
+        int[] array = new int[1];
+        CrivoEratostenes.crivoEratostenes(array, quantidade);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void testArrayPosicaoEsperadaDiferenteZero() {
+        int quantidade = 5;
+        int[] array = {1, 1, 0, 2, 0};
+        CrivoEratostenes.crivoEratostenes(array, quantidade);
+    }
+
     @Test
-    public void testPotencia() {
-        int n = 3;
-        int[] array = new int[n];
-        int[] expResult = {0,1,1};
-        int[] result = CrivoEratostenes.crivoEratostenes(array, n);
+    public void testCrivo() {
+        int quantidade = 10;
+        int[] array = new int[quantidade];
+        for (int i = 0; i < quantidade; i++) {
+            array[i] = (quantidade == 0 || quantidade == 1) ? 1 : 0;
+        }
+        int[] expResult = {1, 1, 1, 0, 1, 0, 1, 0, 0, 0};
+        int[] result = CrivoEratostenes.crivoEratostenes(array, quantidade);
         assertArrayEquals(expResult, result);
     }
-    
+
 }
